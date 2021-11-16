b2a4dbb6493ce04a694097ad0f558e638d80febc
Matt <iammattvaldez@gmail.com>
2021-11-16 14:33:14 -0800

    main structure formalized?

---
+++ rubyserver/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/weight-for-<int:user_id>/", methods=["GET"])
+def get_weight():
+    """
+    Using the UserID provided in the link, this would return a JSON file with the user data and the weight during the
+    month.
+
+    :return: Bytes JSON data
+    """
+    pass
+
+
+@app.route("/send-weight-for-<int:user_id>/", methods=["POST"])
+def post_weight():
+    """
+    Using the UserID provided in the link and a bytes dictionary with the UserID, weight, and current date/time, this
+    would update the user's weight.
+
+    :return: A success/error message.
+    """
+    pass
