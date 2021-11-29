dcab7071cd67e7abbf3de73b56cf503f52a0eec5
Matt <iammattvaldez@gmail.com>
2021-11-29 15:08:59 -0800

    recordWeight, returnDataFrame, and more DataManager() features

--- rubyserver/SupportProtocol.py
+++ rubyserver/SupportProtocol.py
@@ -1,6 +1,9 @@
+from pandas.io.formats.format import Datetime64Formatter
 import pytz
 from datetime import datetime
 import calendar
+import pandas as pd
+import random
 
 
 # Keeping this here so that the proper keys are set without struggle.
@@ -38,3 +41,58 @@ class Now():
         :return: Integer
         """
         return calendar.monthrange(year, month)[1]
+
+
+class DataManager():
+    @staticmethod
+    def create_frame():
+        """
+        This is for developmental purposes only.
+        Creates a brand new set for testing.
+        """
+
+        def create_new_data():
+            return [random.randint(195, 200) for number in range(0, 5)]
+
+        data = pd.DataFrame({
+            "Date": ["2021-11-29", "2021-11-30", "2021-12-01", "2021-12-05", "2021-11-10"],
+            "Matt": create_new_data(),
+            "Carlos": create_new_data(),
+            "Ruby": create_new_data()
+        })
+
+        data.to_csv("rubyserver/data.csv", index=False)
+
+
+    @staticmethod
+    def returnDataFrame():
+        frame = pd.read_csv("rubyserver/data.csv", index_col='Date')
+
+        return frame
+
+
+    @staticmethod
+    def recordWeight(id: str, weight):
+        """
+        Records the weight of the individual.
+        """
+        assert type(weight) is int or type(weight) is float, '"weight" parameter must be either an integer or a float.'
+
+        id = id if type(id) is str else str(id)
+        day = Now().pst.day if Now().pst.day >= 10 else f"0{Now().pst.day}"
+        month = Now().pst.month if Now().pst.month >= 10 else f"0{Now().pst.month}"
+        date = f"{Now().pst.year}-{month}-{day}"
+
+        dataframe = DataManager.returnDataFrame()
+
+        exists = True if date in dataframe.index.values else False
+
+        if not exists:
+            for user in dataframe.columns.values:
+                dataframe.loc[date, user] = 0.0 if user != id else weight
+        else:
+            dataframe.loc[date, id] = weight
+
+        dataframe.to_csv(f"rubyserver/data.csv", index=True, index_label="Date")
+
+DataManager.recordWeight("Ruby", 300)
