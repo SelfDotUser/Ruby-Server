b57060bee409198903bb8bd280bbf3dff2304173
Matt <iammattvaldez@gmail.com>
2021-11-30 14:52:08 -0800

    Satisfying PIP8, adding requirements to pipfile.lock

--- rubyserver/SupportProtocol.py
+++ rubyserver/SupportProtocol.py
@@ -1,4 +1,3 @@
-from pandas.io.formats.format import Datetime64Formatter
 import pytz
 from datetime import datetime
 import calendar
@@ -10,31 +9,29 @@ import random
 isPublic = False
 
 
-class Now():
+class Now:
     def __init__(self):
         utc_now = pytz.utc.localize(datetime.utcnow())
         self.pst = utc_now.astimezone(pytz.timezone('US/Pacific'))
 
-
-    def getDate(self):
+    def get_date(self):
         """
         Returns the date in the PST timezone. "January 1, 2000" format.
         """
-        currentDay = self.pst.strftime("%B %d, %Y")
+        current_day = self.pst.strftime("%B %d, %Y")
 
-        return currentDay
+        return current_day
 
-
-    def getTime(self):
+    def get_time(self):
         """
         Returns the current time in the PST timezone. "01:00 AM" format.
         """
-        currentTime = self.pst.strftime('%I:%M %p')
+        current_time = self.pst.strftime('%I:%M %p')
 
-        return currentTime
+        return current_time
 
-
-    def getDaysInMonth(year:int, month:int):
+    @staticmethod
+    def get_days_in_month(year: int, month: int):
         """
         Returns the number of days there are in the specified month in the specified year.
 
@@ -43,7 +40,7 @@ class Now():
         return calendar.monthrange(year, month)[1]
 
 
-class DataManager():
+class DataManager:
     @staticmethod
     def create_frame():
         """
@@ -52,7 +49,7 @@ class DataManager():
         """
 
         def create_new_data():
-            return [random.randint(195, 200) for number in range(0, 5)]
+            return [random.randint(195, 200) for i in range(0, 5)]
 
         data = pd.DataFrame({
             "Date": ["2021-11-29", "2021-11-30", "2021-12-01", "2021-12-05", "2021-11-10"],
@@ -63,36 +60,35 @@ class DataManager():
 
         data.to_csv("rubyserver/data.csv", index=False)
 
-
     @staticmethod
-    def returnDataFrame():
+    def return_dataframe():
         frame = pd.read_csv("rubyserver/data.csv", index_col='Date')
 
         return frame
 
-
     @staticmethod
-    def recordWeight(id: str, weight):
+    def record_weight(user_id: str, weight):
         """
         Records the weight of the individual.
         """
         assert type(weight) is int or type(weight) is float, '"weight" parameter must be either an integer or a float.'
 
-        id = id if type(id) is str else str(id)
+        user_id = user_id if type(user_id) is str else str(user_id)
         day = Now().pst.day if Now().pst.day >= 10 else f"0{Now().pst.day}"
         month = Now().pst.month if Now().pst.month >= 10 else f"0{Now().pst.month}"
         date = f"{Now().pst.year}-{month}-{day}"
 
-        dataframe = DataManager.returnDataFrame()
+        dataframe = DataManager.return_dataframe()
 
         exists = True if date in dataframe.index.values else False
 
         if not exists:
             for user in dataframe.columns.values:
-                dataframe.loc[date, user] = 0.0 if user != id else weight
+                dataframe.loc[date, user] = 0.0 if user != user_id else weight
         else:
-            dataframe.loc[date, id] = weight
+            dataframe.loc[date, user_id] = weight
 
         dataframe.to_csv(f"rubyserver/data.csv", index=True, index_label="Date")
 
-DataManager.recordWeight("Ruby", 300)
+
+DataManager.record_weight("Ruby", 300)
